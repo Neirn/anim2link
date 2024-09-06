0a309291e5735bc112ecf72bca4798a247b1570f
Neirn <neirny@gmail.com>
2024-09-06 13:11:14 -0700

    rename button3 -> buttonOpenSelectRomDialogue

--- source/Form1.Designer.cs
+++ source/Form1.Designer.cs
@@ -53,7 +53,7 @@
             this.textBox4 = new System.Windows.Forms.TextBox();
             this.label2 = new System.Windows.Forms.Label();
             this.textBox3 = new System.Windows.Forms.TextBox();
-            this.button3 = new System.Windows.Forms.Button();
+            this.buttonOpenSelectRomDialogue = new System.Windows.Forms.Button();
             this.textBoxRomPath = new System.Windows.Forms.TextBox();
             this.timer1 = new System.Windows.Forms.Timer(this.components);
             this.btnRefresh = new System.Windows.Forms.Button();
@@ -234,7 +234,7 @@
             this.groupBox3.Controls.Add(this.textBox4);
             this.groupBox3.Controls.Add(this.label2);
             this.groupBox3.Controls.Add(this.textBox3);
-            this.groupBox3.Controls.Add(this.button3);
+            this.groupBox3.Controls.Add(this.buttonOpenSelectRomDialogue);
             this.groupBox3.Controls.Add(this.textBoxRomPath);
             this.groupBox3.Enabled = false;
             this.groupBox3.Location = new System.Drawing.Point(6, 42);
@@ -295,15 +295,15 @@
             this.textBox3.TabIndex = 6;
             this.textBox3.Text = "0x2310";
             // 
-            // button3
+            // buttonOpenSelectRomDialogue
             // 
-            this.button3.Location = new System.Drawing.Point(152, 17);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(35, 23);
-            this.button3.TabIndex = 5;
-            this.button3.Text = "...";
-            this.button3.UseVisualStyleBackColor = true;
-            this.button3.Click += new System.EventHandler(this.button3_Click);
+            this.buttonOpenSelectRomDialogue.Location = new System.Drawing.Point(152, 17);
+            this.buttonOpenSelectRomDialogue.Name = "buttonOpenSelectRomDialogue";
+            this.buttonOpenSelectRomDialogue.Size = new System.Drawing.Size(35, 23);
+            this.buttonOpenSelectRomDialogue.TabIndex = 5;
+            this.buttonOpenSelectRomDialogue.Text = "...";
+            this.buttonOpenSelectRomDialogue.UseVisualStyleBackColor = true;
+            this.buttonOpenSelectRomDialogue.Click += new System.EventHandler(this.button3_Click);
             // 
             // textBoxRomPath
             // 
@@ -361,7 +361,7 @@
         private System.Windows.Forms.CheckBox checkBox1;
         private System.Windows.Forms.GroupBox groupBox3;
         private System.Windows.Forms.Button button2;
-        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button buttonOpenSelectRomDialogue;
         private System.Windows.Forms.TextBox textBoxRomPath;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.TextBox textBox3;
